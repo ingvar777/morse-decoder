24f1f79d56d83935cb82b5b9354baa8905a3faeb
ingvar777 <90556370+ingvar777@users.noreply.github.com>
2022-01-28 10:32:13 +0500

    feat: mission accomplished

--- src/index.js
+++ src/index.js
@@ -37,8 +37,41 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
+function zeroMorse(prop) {
+
+    let morse = [];
+
+    for (let i = 0; i < prop.length; i = i + 2) {
+        switch (prop.substring(i, i + 2)) {
+            case "10":
+                morse.push(".");
+                break;
+            case "11":
+                morse.push("-");
+                break;
+            default:
+                break;
+        }
+    }
+
+    return MORSE_TABLE[morse.join("")]
+}
+
+
+
 function decode(expr) {
-    // write your solution here
+    if (expr.length % 10) return -1
+
+    let text = "";
+    let charZero = "";
+
+    for (let i = 0; i < expr.length; i = i + 10) {
+
+        charZero = expr.substring(i, i + 10);
+        text = text + ((charZero !== "**********") ? zeroMorse(charZero) : " ");
+    }
+
+    return text
 }
 
 module.exports = {
